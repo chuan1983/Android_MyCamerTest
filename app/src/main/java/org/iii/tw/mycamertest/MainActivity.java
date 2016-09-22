165d4a8b2534d14cdd3464e345a88fdb27d5f887
chuan1983 <chuan1983126@gmail.com>
2016-09-22 14:34:44 +0800

    v2

--- app/src/main/java/org/iii/tw/mycamertest/MainActivity.java
+++ app/src/main/java/org/iii/tw/mycamertest/MainActivity.java
@@ -1,12 +1,19 @@
 package org.iii.tw.mycamertest;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView img;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         img = (ImageView)findViewById(R.id.img);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+//        Log.d("brad",""+resultCode);
+        if(resultCode == RESULT_OK){
+            afterTakePic1(data);
+        }else if(requestCode == RESULT_CANCELED){
+
+        }
+    }
+
+    private void afterTakePic1(Intent data){
+        Bitmap bmp = (Bitmap)data.getExtras().get("data");
+        img.setImageBitmap(bmp);
+    }
+
+    public void test1(View v){
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(it,1);
+    }
+    public void test2(View v){
+
     }
 }
