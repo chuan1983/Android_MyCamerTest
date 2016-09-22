0b8ffec8c6e958bfb50a85b5ce775522c6f5bb3b
chuan1983 <chuan1983126@gmail.com>
2016-09-22 14:44:19 +0800

    v3

--- app/src/main/java/org/iii/tw/mycamertest/MainActivity.java
+++ app/src/main/java/org/iii/tw/mycamertest/MainActivity.java
@@ -2,6 +2,9 @@ package org.iii.tw.mycamertest;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,9 +13,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView img;
+    private File sdroot;
 
 
     @Override
@@ -21,15 +27,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         img = (ImageView)findViewById(R.id.img);
-
+        sdroot = Environment.getExternalStorageDirectory();
     }
+
+    public void test1(View v){
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        Uri outputFile = Uri.fromFile(new File(sdroot,"brad.jpg"));
+        it.putExtra(MediaStore.EXTRA_OUTPUT, outputFile);
+
+        startActivityForResult(it,1);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 //        Log.d("brad",""+resultCode);
         if(resultCode == RESULT_OK){
-            afterTakePic1(data);
+//            afterTakePic1(data);
+            afterTakePic2(data);
         }else if(requestCode == RESULT_CANCELED){
 
         }
@@ -40,10 +57,11 @@ public class MainActivity extends AppCompatActivity {
         img.setImageBitmap(bmp);
     }
 
-    public void test1(View v){
-        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(it,1);
+    private void afterTakePic2(Intent data){
+        Bitmap bmp = BitmapFactory.decodeFile(sdroot.getAbsolutePath() + "/brad.jpg");
+        img.setImageBitmap(bmp);
     }
+
     public void test2(View v){
 
     }
